7adaf0ca7709a975c81d6554852e7fd8eb599690
Rowan <rowanccarson@gmail.com>
2019-05-22 18:46:34 -0700

    order change

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # babysfirstdiscordbot
+https://medium.com/davao-js/2019-tutorial-creating-your-first-simple-discord-bot-47fc836a170b
--- bot.js
+++ bot.js
@@ -5,11 +5,9 @@ const auth = require('./auth.json');
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.login(auth.token);
-
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
 });
+client.login(auth.token);
